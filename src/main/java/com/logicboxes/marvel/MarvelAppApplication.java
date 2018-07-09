9a7d0f2476f2a77dd92b681d50d5a81b77cf9508
kunal.va <kunal.va@endurance.com>
2018-07-09 11:35:46 +0530

    Inception

---
+++ src/main/java/com/logicboxes/marvel/MarvelAppApplication.java
@@ -0,0 +1,12 @@
+package com.logicboxes.marvel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelAppApplication.class, args);
+	}
+}
